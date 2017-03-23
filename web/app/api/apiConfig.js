867ce4cdedd3eff1ade6777b447eafaae44934e2
Ruslan Salikhov <ruslan.salihov@gmail.com>
2017-03-23 23:03:40 +0500

    Added rudex api node

--- web/app/api/apiConfig.js
+++ web/app/api/apiConfig.js
@@ -10,9 +10,10 @@ export const settingsAPIs = {
     WS_NODE_LIST: [
         {url: "wss://fake.automatic-selection.com", location: "Choose closest automatically"},
         {url: "ws://127.0.0.1:8090", location: "Locally hosted"},
-        {url: "wss://bit.btsabc.org/ws", location: "Hong Kong"},
+        {url: "wss://node.market.rudex.org", location: "RuDEX (Germany)"},
         {url: "wss://bitshares.openledger.info/ws", location: "Nuremberg, Germany"},
         {url: "wss://eu.openledger.info/ws", location: "Berlin, Germany"},
+        {url: "wss://bit.btsabc.org/ws", location: "Hong Kong"},
         {url: "wss://bts.transwiser.com/ws", location: "Hangzhou, China"},
         {url: "wss://bitshares.dacplay.org:8089/ws", location:  "Hangzhou, China"},
         {url: "wss://openledger.hk/ws", location: "Hong Kong"},
