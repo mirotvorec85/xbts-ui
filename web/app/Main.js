0d4936f01c15f2e5a463d731020aff090c91f00a
Ruslan Salikhov <ruslan.salihov@gmail.com>
2017-04-02 20:45:56 +0500

    Using 9999.99999 format for numbers

--- web/app/Main.js
+++ web/app/Main.js
@@ -1,11 +1,11 @@
 require("./assets/loader");
-if (!window.Intl) { // Safari polyfill
+//if (!window.Intl) { // Safari polyfill
     require.ensure(["intl"], require => {
         window.Intl = require("intl");
         Intl.__addLocaleData(require("./assets/intl-data/en.json"));
         Intl.__addLocaleData(require("./assets/intl-data/ru.json"));
         require("index.js");
     });
-} else {
-    require("index.js");
-}
+//} else {
+//    require("index.js");
+//}
