2c67a29ba7e2ba995bea7d5addd9eaec7e8c4986
Ruslan Salikhov <ruslan.salihov@gmail.com>
2018-04-03 23:05:58 +0500

    Added chatbro trollbox

---
+++ app/components/Chat/ChatBro.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {PropTypes} from "react";
+import SettingsStore from "stores/SettingsStore";
+
+/**
+ *  Wrapper component for chatbro iframe
+ *
+ */
+class ChatBro extends React.Component {
+    render() {
+        let frameHeight = this.props.height || "30%";
+
+        let currentLocale = SettingsStore.getState().settings.get("locale");
+
+        let chatUrl = null;
+        switch (currentLocale) {
+            default:
+                chatUrl = `https://chat.rudex.org/${currentLocale}/`;
+                break;
+        }
+
+        return (
+            <iframe
+                style={{height: frameHeight, width: "100%"}}
+                src={chatUrl}
+                sandbox="allow-same-origin allow-forms allow-scripts allow-popups"
+            />
+        );
+
+        return (
+            <iframe
+                style={{height: frameHeight, width: "100%"}}
+                src={chatUrl}
+                sandbox="allow-same-origin allow-forms allow-scripts allow-popups"
+            />
+        );
+    }
+}
+
+ChatBro.propTypes = {
+    height: PropTypes.string
+};
+
+export default ChatBro;
